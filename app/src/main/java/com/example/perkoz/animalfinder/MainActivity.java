29ab9dabfaeb2886679cb9133bf61b08dbe95657
pawel2973 <37537809+pawel2973@users.noreply.github.com>
2018-11-27 18:57:42 +0100

    Load Image from Gallery
    Successful

--- app/src/main/java/com/example/perkoz/animalfinder/MainActivity.java
+++ app/src/main/java/com/example/perkoz/animalfinder/MainActivity.java
@@ -1,18 +1,31 @@
 package com.example.perkoz.animalfinder;
 
-        import android.content.Intent;
-        import android.database.Cursor;
-        import android.graphics.BitmapFactory;
-        import android.net.Uri;
-        import android.provider.MediaStore;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.widget.ImageButton;
-        import android.widget.ImageView;
+import android.content.Intent;
+import android.database.Cursor;
+
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
-    private static int RESULT_LOAD_IMAGE = 1;
+    private static final int SELECT_PICTURE = 1;
+
+    private ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,39 +44,42 @@ public class MainActivity extends AppCompatActivity {
 
 
         // -------------------- LOAD IMAGE BUTTON --------------------
-        ImageButton galleryImageButton = findViewById(R.id.imageButtonGallery);
-        galleryImageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(
-                        Intent.ACTION_PICK,
-                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(i, RESULT_LOAD_IMAGE);
-            }
-        });
+        img = (ImageView) findViewById(R.id.imageView);
+
+        findViewById(R.id.imageButtonGallery)
+                .setOnClickListener(new OnClickListener() {
+                    public void onClick(View arg0) {
+                        Intent intent = new Intent();
+                        intent.setType("image/*");
+                        intent.setAction(Intent.ACTION_GET_CONTENT);
+                        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
+                    }
+                });
     }
     // ### End of OnCreate ###
 
     // -------------------- LOAD IMAGE CODE --------------------
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
-            Uri selectedImage = data.getData();
-            String[] filePathColumn = {MediaStore.Images.Media.DATA};
-
-            Cursor cursor = getContentResolver().query(selectedImage,
-                    filePathColumn, null, null, null);
-            cursor.moveToFirst();
-
-            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            String picturePath = cursor.getString(columnIndex);
-            cursor.close();
-
-            ImageView imageView = findViewById(R.id.imageView);
-            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
-
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        String selectedImagePath;
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                Uri selectedImageUri = data.getData();
+                selectedImagePath = getRealPathFromURI(selectedImageUri);
+                System.out.println("Image Path : " + selectedImagePath);
+                img.setImageURI(selectedImageUri);
+            }
         }
+    }
+
+    public String getRealPathFromURI(Uri contentUri) {
+        String res = null;
+        String[] proj = { MediaStore.Images.Media.DATA };
+        Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null);
+        if(cursor.moveToFirst()){
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            res = cursor.getString(column_index);
+        }
+        cursor.close();
+        return res;
     }
 }
