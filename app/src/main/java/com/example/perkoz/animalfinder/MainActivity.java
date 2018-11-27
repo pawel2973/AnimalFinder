cd4bf99cdfc13d6fb2c927933eaeac21a421ab83
pawel2973 <37537809+pawel2973@users.noreply.github.com>
2018-11-27 18:12:38 +0100

    Upload Image v1
    Not Working

--- app/src/main/java/com/example/perkoz/animalfinder/MainActivity.java
+++ app/src/main/java/com/example/perkoz/animalfinder/MainActivity.java
@@ -1,18 +1,25 @@
 package com.example.perkoz.animalfinder;
 
-import android.media.Image;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
+        import android.content.Intent;
+        import android.database.Cursor;
+        import android.graphics.BitmapFactory;
+        import android.net.Uri;
+        import android.provider.MediaStore;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.ImageButton;
+        import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private static int RESULT_LOAD_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // -------------------- EXIT BUTTON --------------------
         ImageButton exitButton = findViewById(R.id.imageButtonExit);
         exitButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -21,5 +28,42 @@ public class MainActivity extends AppCompatActivity {
                 System.exit(0);
             }
         });
+
+
+        // -------------------- LOAD IMAGE BUTTON --------------------
+        ImageButton galleryImageButton = findViewById(R.id.imageButtonGallery);
+        galleryImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
+    }
+    // ### End of OnCreate ###
+
+    // -------------------- LOAD IMAGE CODE --------------------
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            ImageView imageView = findViewById(R.id.imageView);
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+        }
     }
 }
